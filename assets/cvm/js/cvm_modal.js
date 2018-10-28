8da3e46c0d56a4b1d59bb97524a9b8c988aa7502
Christophe Van Maercke <vanmaerckechri@gmail.com>
2018-10-29 00:07:01 +0100

    update modal

--- assets/cvm/js/cvm_modal.js
+++ assets/cvm/js/cvm_modal.js
@@ -2,39 +2,38 @@
 
 document.addEventListener("DOMContentLoaded", function(event)
 {
-	let modalContainerList = document.querySelectorAll(".modal-container");
 	let modalButtonList = document.querySelectorAll(".modal-btn");
-	let ctrLength = modalContainerList.length;
 	let btnLength = modalButtonList.length;
 
 	for (let i = btnLength - 1; i >= 0; i--)
 	{
-		// check if modal id button start with "modal-btn_"
-		let idModalBtn = modalButtonList[i].id;
-		if (idModalBtn.indexOf("modal-btn_") != -1)
+		// check if modal button has a class that start with "target_"
+		modalButtonList[i]["classList"].forEach(function(className)
 		{
-			// get modal container (container.id = button.id - "modal-btn_")
-			let idModalCtr = idModalBtn.slice(10, idModalBtn.length);
-			let modalContainer = document.getElementById(idModalCtr);
-
-			if (modalContainer.classList.contains("modal-container"))
+			if (className.indexOf("target_") != -1)
 			{
-				// open/close button
-				modalButtonList[i].addEventListener("click", function()
+				// get modal container by id from part of the class button
+				let idModalCtr = className.slice(7, className.length);
+				let modalContainer = document.getElementById(idModalCtr);
+				if (modalContainer.classList.contains("modal-container"))
 				{
-					modalContainer.classList.toggle("hidden");
-				}, false);
-
-				// close (if click outside content)
-				modalContainer.addEventListener("click", function(event)
-				{
-					if (modalContainer == event.target)
+					// open/close button
+					modalButtonList[i].addEventListener("click", function()
 					{
 						modalContainer.classList.toggle("hidden");
-					}
-				}, false);
+					}, false);
+
+					// close (if click outside content)
+					modalContainer.addEventListener("click", function(event)
+					{
+						if (modalContainer == event.target)
+						{
+							modalContainer.classList.toggle("hidden");
+						}
+					}, false);
+				}
 			}
-		}
+		});
 	}
 });
 
@@ -46,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function(event)
     <div id="name01" class="modal-container hidden">
 		...
     </div>
-    <button id="modal-btn_name01" class="modal-btn">open</button>
+    <button id="modal-btn" class="modal-btn target_name01">open</button>
 
     <script type="text/javascript" src="assets/cvm/js/cvm_modal.js"></script>
 */
